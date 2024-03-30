3e68d8041f3d05de2ae99c9a93e86379237399e6
Ashu-void <ashudking64@gmail.com>
2024-03-30 13:57:11 +0530

    Weather App

---
+++ app/src/main/java/com/first/weatherapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.first.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//3f285df0e804498b99fdaeeeff9445fa
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
